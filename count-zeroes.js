3a09f3973fb4b723403ed2b539822f626d94e067
sam <wax654@gmail.com>
2021-08-07 13:14:25 -0700

    Finished count zeros

--- count-zeroes.js
+++ count-zeroes.js
@@ -1,5 +1,38 @@
-function countZeroes() {
-  
+function countZeroes(arr) {
+    let testIdx = Math.floor(arr.length - 1/2);
+    let loops = 0;
+    while(true){
+        loops++;
+        if(arr[testIdx] === 0){
+            if(testIdx === 0){
+                console.log('loops', loops);
+                return arr.length;
+            }
+            if(arr[testIdx - 1] === 1){
+                console.log('loops', loops);
+                return arr.length - testIdx;
+            } else {
+                testIdx = Math.floor(testIdx/2);
+            }
+        } else {
+            if(testIdx === arr.length - 1){
+                console.log('loops', loops);
+                return 0;
+            }
+            else if(arr[testIdx + 1] === 0) {
+                console.log('loops', loops);
+                return arr.length - (testIdx + 1);
+            }
+            else {
+                //set the idx to the middle between the end and the current idx
+                testIdx = Math.floor(((testIdx + (arr.length-1))/2));
+            }
+        }
+    }
 }
-
+console.log(countZeroes([1, 1, 1, 1, 0, 0])); // 2
+console.log(countZeroes([1, 0, 0, 0, 0])) // 4
+console.log(countZeroes([0, 0, 0])) // 3
+console.log(countZeroes([1, 1, 1, 1])) // 0
+console.log(countZeroes([1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0])) // 0
 module.exports = countZeroes
--- sorted-frequency.js
+++ sorted-frequency.js
@@ -1,5 +1,8 @@
 function sortedFrequency() {
 
 }
-
+console.log(sortedFrequency([1, 1, 2, 2, 2, 2, 3], 2)) // 4
+console.log(sortedFrequency([1, 1, 2, 2, 2, 2, 3], 3)) // 1
+console.log(sortedFrequency([1, 1, 2, 2, 2, 2, 3], 1)) // 2
+console.log(sortedFrequency([1, 1, 2, 2, 2, 2, 3], 4)) // -1
 module.exports = sortedFrequency
